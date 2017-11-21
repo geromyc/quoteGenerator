8dad28f51eaacf45be1ad91d146328518aac925e
Geromy Cunningham <geromycunningham@gmail.com>
2017-11-21 21:56:52 +0000

    hacker 1 challenges complete

--- script.js
+++ script.js
@@ -15,14 +15,39 @@ var quotes = {
     postTwitter: function() {
         window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent('"' + quotes.quote + '"' + '  - ' + quotes.title));
     },
+    postReddit: function() {
+        window.open('https://www.reddit.com/r/quotes/submit?amp;title=' + "Quote by: " + quotes.title + "&text=" + quotes.quote);
+    },
+    colorChange: function() {
+      var r = Math.floor(Math.random() * 256);
+      var g = Math.floor(Math.random() * 256);
+      var b = Math.floor(Math.random() * 256);
+      var rgb = 'rgb(' + r + ',' + g + ',' + b + ')';
+      document.getElementById("quoteDisp").style.color = rgb;
+      document.getElementById("authorDisp").style.color = rgb;
+    },
 };
 
-function getQuote() {
+
+
+document.getElementById("newQuote").onclick = function() {
   quotes.generQuote();
-};
-function postTwitter() {
+  quotes.colorChange();
+}
+document.getElementById("postTwit").onclick = function() {
   quotes.postTwitter();
-};
-function postReddit() {
+}
+document.getElementById("postRed").onclick = function() {
   quotes.postReddit();
-};
+}
+
+
+// function newQuote() {
+//   quotes.generQuote();
+// }
+// function postTwitter() {
+//   quotes.postTwitter();
+// };
+// function postReddit() {
+//   quotes.postReddit();
+// };
